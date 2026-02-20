de5111e99d18939fa6c7c522056978c81fae0b68
notnico22 <notnicotv@gmail.com>
2026-02-20 02:20:31 -0500

    Deleted Old Local Settings --> Moved To GameOptions

--- DraftModeLocalSettings.cs
+++
@@ -1,5 +0,0 @@
-// This file is intentionally empty.
-// All Draft Mode settings have been moved to DraftModeOptions (host game options).
-// The "Start Draft" button is now handled via ChatPatch (/draft command) or the host can use
-// DraftManager.StartDraft() directly.
-namespace DraftModeTOUM { }
